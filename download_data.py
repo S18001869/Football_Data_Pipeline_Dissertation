efa6d809807a7ca2322d797020dc83a9600aa168
S18001869 <47390846+S18001869@users.noreply.github.com>
2021-03-09 12:37:05 +0000

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -4,19 +4,23 @@ def generate_download_links(start_year=2018, end_year=2020):
     premier league data from football-data.co.uk"""
 
     # "https://www.football-data.co.uk/mmz4281/2021/E0.csv"
+    # Create array containing all relevant URLs
+    data_links = ["https://www.football-data.co.uk/mmz4281/2021/E0.csv",
+                  "https://www.football-data.co.uk/mmz4281/1920/E0.csv",
+                  "https://www.football-data.co.uk/mmz4281/1819/E0.csv"]
 
     output = list()
 
     num_loops = end_year - start_year
     season_start_year = str(start_year)[-2:]
     season_start_end = str(int(season_start_year)+1)
-    season_start_year + season_start_end
 
     # DO STUFF HERE
-    for i in range(num_loops):
+    for i in data_links:
         # In a loop
-        year_string = str(2018)
-        link = f"https://www.football-data.co.uk/mmz4281/{year_string}/E0.csv"
+        start_year_string = str(season_start_year)
+        end_year_string = str(int(season_start_year)+1)
+        link = f"https://www.football-data.co.uk/mmz4281/{start_year_string}{end_year_string}/E0.csv"
         output.append(link)
-
     return output
+
