3603a61cf99bd27acd31e58cc434e380bc470202
S18001869 <47390846+S18001869@users.noreply.github.com>
2021-03-25 17:52:52 +0000

    Changes to main.py
    - converted main.py into function
    - created a loop to prevent table from dropping after each iteration

--- download_data.py
+++ download_data.py
@@ -1,32 +1,30 @@
-def generate_download_links(start_year=2018, end_year=2020):
+def generate_download_links(start_year=2018, end_year=2020): # defining the function
     """Given a start year and an end year, generate a list of links for
     premier league data from football-data.co.uk"""
 
-    # "https://www.football-data.co.uk/mmz4281/2021/E0.csv"
-    # Create array containing all relevant URLs
-
-    output = list()
+    output = list()  # defining an empty list to use later
 
     # Find out how many links to generate
     num_loops = end_year - start_year
 
-    for i in range(num_loops):
+    for i in range(num_loops): # For loop to generate the links dynamically (automatically)
         # Generate the year_string variable
         season_start_year = str(start_year + i)[-2:]
         season_start_end = str(int(season_start_year) + 1).zfill(2)
         year_string = season_start_year + season_start_end
         link = f"https://www.football-data.co.uk/mmz4281/{year_string}/E0.csv"
 
-        output.append(link)
+        output.append(link) # updating the output list with the new string
     return output
 
 
-def test_generate_download_links():
+def test_generate_download_links(): # Here we are testing the function
     expected_output = [
         'https://www.football-data.co.uk/mmz4281/1819/E0.csv',
         'https://www.football-data.co.uk/mmz4281/1920/E0.csv'
     ]
     assert generate_download_links(2018, 2020) == expected_output, "Test failed"
+    # If the output is equal to the expected output then the test passes
     print("Test passed")
 
 
@@ -36,12 +34,15 @@ def test_generate_download_links_one_digit_year():
         'https://www.football-data.co.uk/mmz4281/0203/E0.csv'
     ]
     assert generate_download_links(2001, 2003) == expected_output, "Test failed"
+    # Test whether the calculation works for years prior to 2010
     print("Test passed")
 
 
 if __name__ == '__main__':
+    # Prevents the code beneath from running all code if I'm importing just one function, rather than the whole code
+    # __name__ refers to the script that's being run
     test_generate_download_links()
     test_generate_download_links_one_digit_year()
-    links = generate_download_links(2001, 2020)
+    links = generate_download_links(2001, 2003)
     # for link in links:
     # get_football_data(link)
--- main.py
+++ main.py
@@ -10,58 +10,82 @@ import pandas as pd
 # We put all functions inside their own script to make this script cleaner
 from utils import connect_to_db, run_query, convert_data_type_names, clean_query
 
-# Load data
-df = pd.read_csv('data.csv')
-# Define the name of the table to hold the data
-table_name = 'results'
+from download_data import generate_download_links
+
 # Define location of the db
 db_dir = 'db.sqlite'
-# Create connection to DB
-conn = connect_to_db('db.sqlite')
-# Create the cursor (which you use to run queries)
-cursor = conn.cursor()
+# Define the name of the table to hold the data
+table_name = 'results'
 
-# Convert column names to lower case so the names are easier to write.
-# The code below is called a 'list comprehension', google it
-df.columns = [col.lower() for col in df.columns]
-# Convert 'as' to 'away_shots' and 'hs' to 'home_shots'
-# We do this because 'as' is a command in SQL so it cant be a column name
-df = df.rename(columns={'as': 'away_shots', 'hs': 'home_shots'})
-# Get the data types for each of the columns
-data_types = dict(df.dtypes)
 
-# Create a description of the columns, used to create the table. Print this to see how it looks.
-# This is pretty complicated so dont worry about understanding it, you can also write this by hand as a string
-column_description = ''.join([f"{key} {convert_data_type_names(value)}, " for key, value in data_types.items()])
-# Remove the ", " from the end of the description
-column_description = column_description[:-2]
+def GetResults(link, dropresults):
 
-# Clear the table if it already exists
-run_query(query=f'DROP TABLE IF EXISTS {table_name}', return_data=False, path_to_db=db_dir)
-# Create the table
-query = f"CREATE TABLE {table_name} ({column_description})"
-# Remove any non-alphabet symbols
-query = clean_query(query)
-run_query(query=query, return_data=False, path_to_db=db_dir)
+    # Load data
+    df = pd.read_csv(link)
+    # Create connection to DB
+    conn = connect_to_db('db.sqlite')
+    # Create the cursor (which you use to run queries)
+    cursor = conn.cursor()
 
-# Upload the results to a table, row by row. We do this by taking the keys of the dictionary,
-# and joining them together
-column_names = ', '.join([key for key, value in data_types.items()])
+    # Convert column names to lower case so the names are easier to write.
+    # The code below is called a 'list comprehension', google it
+    df.columns = [col.lower() for col in df.columns]
+    # Convert 'as' to 'away_shots' and 'hs' to 'home_shots'
+    # We do this because 'as' is a command in SQL so it cant be a column name
+    df = df.rename(columns={'as': 'away_shots', 'hs': 'home_shots'})
+    # Get the data types for each of the columns
+    data_types = dict(df.dtypes)
 
-for i in range(len(df)):
-    # Get the i'th row, convert it to a list
-    row = list(df.iloc[i, :])
-    for j in range(len(df.columns)):
-        data_type = data_types[df.columns[j]]
-        if str(data_type) == 'object':
-            row[j] = "'" + str(row[j]) + "'"
+    # Create a description of the columns, used to create the table. Print this to see how it looks.
+    # This is pretty complicated so dont worry about understanding it, you can also write this by hand as a string
+    column_description = ''.join([f"{key} {convert_data_type_names(value)}, " for key, value in data_types.items()])
+    # Remove the ", " from the end of the description
+    column_description = column_description[:-2]
 
-    # Convert all elements of the list to strings
-    string_row = ", ".join([str(x) for x in row])
-    # Add the values to the database
-    query = f"INSERT INTO {table_name} ({column_names}) VALUES ({string_row})"
-    query = clean_query(query)
-    run_query(query=query, return_data=False, path_to_db=db_dir)
+    # if statement to decide whether to clear results table or not
 
-# Check that the values have been added
-result = run_query(query=f'select * from {table_name}', return_data=True, path_to_db=db_dir)
+    if dropresults == True:
+        # Clear the table if it already exists
+        run_query(query=f'DROP TABLE IF EXISTS {table_name}', return_data=False, path_to_db=db_dir)
+        # Create the table
+        query = f"CREATE TABLE {table_name} ({column_description})"
+        # Remove any non-alphabet symbols
+        query = clean_query(query)
+        run_query(query=query, return_data=False, path_to_db=db_dir)
+
+    # Upload the results to a table, row by row. We do this by taking the keys of the dictionary,
+    # and joining them together
+    column_names = ', '.join([key for key, value in data_types.items()])
+
+    for i in range(len(df)):
+        # Get the i'th row, convert it to a list
+        row = list(df.iloc[i, :])
+        for j in range(len(df.columns)):
+            data_type = data_types[df.columns[j]]
+            if str(data_type) == 'object':
+                row[j] = "'" + str(row[j]) + "'"
+
+        # Convert all elements of the list to strings
+        string_row = ", ".join([str(x) for x in row])
+        # Add the values to the database
+        query = f"INSERT INTO {table_name} ({column_names}) VALUES ({string_row})"
+        query = clean_query(query)
+        run_query(query=query, return_data=False, path_to_db=db_dir)
+
+
+if __name__ == '__main__':
+
+    dlinks = generate_download_links(2018, 2020)
+    dropcondition = 0 # Set dropcondition to 0
+
+    for footballlinks in dlinks: # For every element in dlinks, call GetResults and pass the link
+        if dropcondition == 0:
+            dropresults = True
+        else:
+            dropresults = False
+        dropcondition = dropcondition + 1 # Add 1 to drop condition each time
+
+        GetResults(footballlinks, dropresults)
+
+    # Check that the values have been added
+    result = run_query(query=f'select * from {table_name}', return_data=True, path_to_db=db_dir)
