8c6a808675f393413938c4a971467a72cd242ccc
Christopher Collins <chris.collins@glovoapp.com>
2021-03-26 16:27:18 +0000

    fix bug in data generation script

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ def GetResults(link, dropresults):
     column_description = column_description[:-2]
 
     # if statement to decide whether to clear results table or not
-
     if dropresults == True:
         # Clear the table if it already exists
         run_query(query=f'DROP TABLE IF EXISTS {table_name}', return_data=False, path_to_db=db_dir)
@@ -52,19 +51,38 @@ def GetResults(link, dropresults):
         # Remove any non-alphabet symbols
         query = clean_query(query)
         run_query(query=query, return_data=False, path_to_db=db_dir)
+        table_cols = list(data_types.keys())
+    else:
+        # TODO: Get a list of columns that exist in the table, if we arent recreating it
+        res = run_query(query=f'select * from {table_name} limit 1', return_data=True, path_to_db=db_dir)
+        data_types2 = dict(res.dtypes)
+        table_cols = list(res.columns)
 
     # Upload the results to a table, row by row. We do this by taking the keys of the dictionary,
     # and joining them together
-    column_names = ', '.join([key for key, value in data_types.items()])
+    column_names = ', '.join(table_cols)
+    # Select all columns stored in table_cols, drop everything else
+
+    # Add any columns that exist in the sql table, but dont exist in the new csv
+    missing_columns = [col for col in table_cols if col not in df.columns]
+    for col in missing_columns:
+        df[col] = 'NULL'
+
+    # Drop any columns that exist in the new csv, but not in the sql table.
+    df = df[table_cols]
+
+    # Combine the schema of the csv file and the sql file (if we are adding data)
+    if dropresults == False:
+        data_types = {**data_types, **data_types2}
 
     for i in range(len(df)):
         # Get the i'th row, convert it to a list
         row = list(df.iloc[i, :])
         for j in range(len(df.columns)):
+            # IF the data type is a string, we need to add ' ' around it
             data_type = data_types[df.columns[j]]
             if str(data_type) == 'object':
                 row[j] = "'" + str(row[j]) + "'"
-
         # Convert all elements of the list to strings
         string_row = ", ".join([str(x) for x in row])
         # Add the values to the database
@@ -78,12 +96,12 @@ if __name__ == '__main__':
     dlinks = generate_download_links(2018, 2020)
     dropcondition = 0 # Set dropcondition to 0
 
-    for footballlinks in dlinks: # For every element in dlinks, call GetResults and pass the link
+    for footballlinks in dlinks:  # For every element in dlinks, call GetResults and pass the link
         if dropcondition == 0:
             dropresults = True
         else:
             dropresults = False
-        dropcondition = dropcondition + 1 # Add 1 to drop condition each time
+        dropcondition = dropcondition + 1  # Add 1 to drop condition each time
 
         GetResults(footballlinks, dropresults)
 
